5cdc8c45b4a1c1c2af75942f209c2f7125b8d0b1
Lucas Montesino <lucasmontesino@MacBook-Air-de-Lucas.local>
2022-09-21 14:29:37 -0300

    control props no terminado

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components"
+import { Product } from '../interfaces/interfaces'
 import '../styles/custom-styles.css'
 
 const product = {
@@ -7,7 +9,21 @@ const product = {
   img: './coffee-mug.png'
 }
 
+const product2 = {
+  id: '1',
+  title: 'Coffe Mug 2',
+  img: './coffee-mug2.png'
+}
+
+const products: Product[] = [ product, product2 ]
+
+// interface ShoppingCartProduct extends Product {
+//   count: number;
+// }
+
 export const ShoppingPage = () => {
+  // const [ shoppingCart, setShoppingCart ] = useState<{ [key: string]: ShoppingCartProduct }>({})
+
   return (
     <div>
       <h1>ShoppingPage</h1>
@@ -20,7 +36,19 @@ export const ShoppingPage = () => {
         flexWrap: 'wrap',
         justifyContent: 'space-evenly',
       }}>
-        <ProductCard product={ product }>
+
+        <div className="shopping-cart">
+          {
+            [1, 2].map((p, i) => (
+              <ProductCard key={ product.id } product={ product } className='algo'>
+                <ProductCard.Image />
+                <ProductCard.Buttons />
+              </ProductCard>
+            ))
+          }
+
+        </div>
+        {/* <ProductCard product={ product }>
           <ProductImage />
           <ProductTitle />
           <ProductButtons />
@@ -30,15 +58,19 @@ export const ShoppingPage = () => {
           <ProductCard.Image />
           <ProductCard.Title />
           <ProductCard.Buttons />
-        </ProductCard>
+        </ProductCard> */}
 
-        <ProductCard product={ product } styles={{
-          backgroundColor: '#544569'
-        }}>
-          <ProductCard.Image />
-          <ProductCard.Title />
-          <ProductCard.Buttons />
-        </ProductCard>
+        {
+          products.map( product => (
+            <ProductCard key={ product.id } product={ product } styles={{
+              backgroundColor: '#544569'
+            }}>
+              <ProductCard.Image />
+              <ProductCard.Title />
+              <ProductCard.Buttons />
+            </ProductCard>
+          ))
+        }
       </div>
     </div>
   )
